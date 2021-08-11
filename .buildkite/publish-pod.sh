92697d5894fb37002eb56296d7bd3cf9829e3e09
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2021-08-11 16:41:35 -0600

    Add Buildkite
    Fix pipeline

---
+++ .buildkite/publish-pod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eu
+
+PODSPEC_PATH="WPMediaPicker.podspec"
+SLACK_WEBHOOK=$PODS_SLACK_WEBHOOK
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :cocoapods: Publishing Pod to CocoaPods CDN"
+publish_pod $PODSPEC_PATH
+
+echo "--- :slack: Notifying Slack"
+slack_notify_pod_published $PODSPEC_PATH $SLACK_WEBHOOK
