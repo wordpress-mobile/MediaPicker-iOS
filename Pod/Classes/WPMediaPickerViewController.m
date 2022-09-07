eafcbd76549421bcb9dbd53ffd5c6c5641b23259
Tony Li <tony.li@automattic.com>
2022-09-07 17:44:18 +1200

    Reload items along with other updates in batch update block

--- Pod/Classes/WPMediaPickerViewController.m
+++ Pod/Classes/WPMediaPickerViewController.m
@@ -688,6 +688,9 @@ static CGFloat SelectAnimationTime = 0.2;
         if ([inserted count] > 0) {
             [self.collectionView insertItemsAtIndexPaths:[self indexPathsFromIndexSet:inserted section:0]];
         }
+        if ([changed count] > 0) {
+            [self.collectionView reloadItemsAtIndexPaths:[self indexPathsFromIndexSet:changed section:0]];
+        }
         for (id<WPMediaMove> move in moves) {
             [self.collectionView moveItemAtIndexPath:[NSIndexPath indexPathForItem:[move from] inSection:0]
                                          toIndexPath:[NSIndexPath indexPathForItem:[move to] inSection:0]];
@@ -697,9 +700,7 @@ static CGFloat SelectAnimationTime = 0.2;
         }
     } completion:^(BOOL finished) {
         [weakSelf refreshSelection];
-        NSMutableSet<NSIndexPath *> *indexPaths = [NSMutableSet setWithArray:[weakSelf indexPathsFromIndexSet:changed section:0]];
-        [indexPaths addObjectsFromArray:weakSelf.collectionView.indexPathsForSelectedItems];
-        [weakSelf.collectionView reloadItemsAtIndexPaths:[indexPaths allObjects]];
+        [weakSelf.collectionView reloadItemsAtIndexPaths:weakSelf.collectionView.indexPathsForSelectedItems];
     }];
 
 }
